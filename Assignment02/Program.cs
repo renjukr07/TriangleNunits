432cdddf57e53ad63a35a09ff7d9af5417c996ae
Sree Renjini Kolappillil Rajendran <renjukr07@gmail.com>
2021-02-24 21:07:57 -0500

    Adding TriangleSolver class

---
+++ Assignment02/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment02
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            int a, b, c;
+            int option;
+            
+            do
+            {
+                Console.Write("\nMenu\n1.Enter Triangle dimensions\n2.Exit\n");
+                option = Convert.ToInt32(Console.ReadLine());
+                if (option == 1)
+                {
+                    Console.WriteLine("Please enter 3 input integers\n");
+                    a = Convert.ToInt32(Console.ReadLine());
+                    b = Convert.ToInt32(Console.ReadLine());
+                    c = Convert.ToInt32(Console.ReadLine());
+                    Console.WriteLine(TriangleSolver.Analyze(a, b, c));
+                }
+                else if(option == 2)
+                {
+                    System.Environment.Exit(0);
+                }
+                else
+                {
+                    Console.WriteLine("Please choose an option from the menu\n");
+                }
+
+
+            } while (option != 2);
+
+            //Console.WriteLine(output);
+            Console.ReadLine();
+
+
+        }
+    }
+}
---
+++ Assignment02/TriangleSolver.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment02
+{
+    public static class TriangleSolver
+    {
+        public static String Analyze(int a, int b, int c)
+        {
+            String output;
+
+            if (a+b>c && b+c>a && a+c> b)
+            {
+                if (a == b && b == c)
+                {
+                    output = "Triangle is Equilateral";
+                }
+                else if((a == b || b == c || c == a)) {
+                    output = "Triangle is Isoceles";
+                }
+                else
+                {
+                    output = "Triangle is Scalene";
+                }
+
+            }
+            else
+            {
+                output = "Input values do not form a triangle";
+                //Console.WriteLine("Input values do not form a triangle\n");
+
+
+            }
+            return output;
+
+
+        }
+
+    }
+}
